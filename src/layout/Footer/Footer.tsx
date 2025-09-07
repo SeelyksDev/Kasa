ee05271eaffc805b876c434434ceb37181c602ac
Kevin <kevincordier.dev@gmail.com>
2025-09-07 19:09:09 +0200

    chore: initialize project structure

---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+    const year = new Date().getFullYear();
+
+    return (
+        <>
+            <img src="" alt="" />
+            <p>© {year} Kasa. All rights reserved</p>
+        </>
+    );
+}
+
+export default Footer;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Rental from "./pages/Rental/Rental";
+import Error404 from "./pages/404/404";
+import Header from "./layout/Header/Header";
+import Footer from "./layout/Footer/Footer";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/rental" element={<Rental />} />
+                <Route path="*" element={<Error404 />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    </StrictMode>
+);
