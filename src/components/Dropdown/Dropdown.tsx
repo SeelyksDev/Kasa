d73cf21fbe22da6dfa97a283fac1809a841e593e
Kevin <kevincordier.dev@gmail.com>
2025-10-01 00:58:45 +0200

    refactor(dropdown): clean up dropdown components

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,19 +1,26 @@
+import { useState } from "react";
 import arrow from "../../assets/svg/dropdown-arow.svg";
 import type { DropdownProps } from "../../utils/types/dropdown.types";
 import "./Dropdown.scss";
 
 function Dropdown({ title, content }: DropdownProps) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggleDropdown () {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <li className="dropdown-item">
-            <details>
-                <summary>
-                    <span>{title}</span>
-                    <img
-                        src={arrow}
-                        alt="flèche dropdown"
-                        className="arrow-dropdown"
-                    />
-                </summary>
+            <div className="dropdown-header" onClick={toggleDropdown}>
+                <span>{title}</span>
+                <img
+                    src={arrow}
+                    alt="flèche dropdown"
+                    className={`arrow-dropdown ${isOpen ? "open" : ""}`}
+                />
+            </div>
+            <div className={`dropdown-content ${isOpen ? "open" : ""}`}>
                 {Array.isArray(content) ? (
                     <ul>
                         {content.map((el, index) => (
@@ -23,7 +30,7 @@ function Dropdown({ title, content }: DropdownProps) {
                 ) : (
                     <p>{content}</p>
                 )}
-            </details>
+            </div>
         </li>
     );
 }
